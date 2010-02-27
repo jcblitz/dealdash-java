5e4c9d1215f5690924f9f1e458d1d21f37cc66ee
Administrator <Administrator@10.0.1.154>
2010-02-26 21:15:47 -0500

    Initial check in of project

---
+++ src/main/java/net/blitzstein/production/dealdash/controllers/DashboardController.java
@@ -0,0 +1,19 @@
+package net.blitzstein.production.dealdash.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ *
+ * @author Jared Blitzstein
+ */
+public class DashboardController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest hsr, HttpServletResponse hsr1) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/layouts/dashboard.jsp
@@ -0,0 +1,13 @@
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
+  <head>
+    <title><tiles:getAsString name="title" /></title>
+  </head>
+  <body>
+    <p>This is a minimal <a href="http://www.w3.org/TR/xhtml11">XHTML 1.1</a> document.</p>
+  </body>
+</html>
